db76b12b5b27120ad57232c68b58784457d4b2d0
notbaopham <113058628+notbaopham@users.noreply.github.com>
2025-03-06 16:48:35 -0800

    Implemented animated coin counter

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int score = 0;
-    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private CoinCounterUI coinCounter;
 
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        scoreText = GetComponent<TextMeshProUGUI>();
     }
 
     public void IncreaseScore()
     {
         score++;
-        scoreText.text = $"Score: {score}";
+        coinCounter.UpdateScore(score);
     }
 }
